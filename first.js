314727bd02625bf0c8310fabefe85d0cfcae2191
DIVEJ131203 <147057830+DIVEJ131203@users.noreply.github.com>
2023-12-16 21:06:49 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Apna College");
+console.log("Divej Ahuja");
+console.log("I love js");
+name="Divej Ahuja";
+console.log(name);
+var age=24;
+console.log(age);
+var price=99.99;
+console.log(price);
+let x=null;
+//let stops re-declaration of variables
+let y=undefined;
+console.log(x);
+console.log(y);
---
+++ practiceset1.js
@@ -0,0 +1,16 @@
+//Queston 1
+const product= {
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
+//Question 2
+const profile={
+    username:"@divejahuja",
+    isFollow:false,
+    followers:35,
+    following:43,
+};
+console.log(profile);
